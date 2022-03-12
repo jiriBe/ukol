b91ab5b4ccd904e221212ce36201d3a18384f0df
jiriBe <87712387+jiriBe@users.noreply.github.com>
2022-03-12 12:23:30 +0100

    Update Code.php

--- class/func/Code.php
+++ class/func/Code.php
@@ -10,8 +10,8 @@ class Code {
 
         $outputData = new Variables\OutputData;
 
-        $outputData->result = md5(mt_rand(0, 99999999999999999) . '_' . time());
-        $outputData->result = mb_substr($outputData->result, mt_rand(5, 12), mt_rand(20, 41));
+        $outputData->result = md5(mt_rand(0, 99999999999999999) . time());
+        $outputData->result = mb_substr($outputData->result, mt_rand(5, 12), mt_rand(20, 32));
 
         return $outputData->result;
 
