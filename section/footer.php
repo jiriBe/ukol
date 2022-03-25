da9dd7779dccfe169baa625c9ced64fb96d0106c
Jiri Be <jiri.beke@seznam.cz>
2022-03-25 23:30:10 +0100

    Update footer.php

--- section/footer.php
+++ section/footer.php
@@ -1,2 +1,2 @@
-<span class="copyLeft">&copy;</span> (CopyLeft) <?php echo date("Y") > 2022 ? '2021 - ' . date("Y") : date("Y") ?>, Jiří
+<span class="copyLeft">&copy;</span> (CopyLeft) <?php echo date("Y") > 2022 ? '2022 - ' . date("Y") : date("Y") ?>, Jiří
 Beke, +420 776 03 06 02
